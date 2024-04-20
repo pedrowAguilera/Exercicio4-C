2857c6424008e18a8ff9b9be7b9dff21046e43df
ppz1 <133365100+pedrowAguilera@users.noreply.github.com>
2024-04-19 23:00:35 -0300

    Initial commit

---
+++ entrada2/entrada2.cpp
@@ -0,0 +1,149 @@
+#include <iostream> <locate>
+
+
+using namespace std;
+
+// ESTUDO DA SALA, DEIXEI AQUI PQ NO OUTRO ARQUIVO NÃO TAVA DANDO CERTO :D
+//int main() {
+//		setlocale(LC_ALL, "pt_BR.UTF-8");
+//
+//		int num = 5;
+//		switch (num)
+//		{
+//		case 1:
+//			cout << "É o numero 1";
+//			break;
+//		default:
+//			cout << "É outro número";
+//			break;
+//		}
+
+
+
+// EXERCICIO 1
+//int main() {
+//	setlocale(LC_ALL, "pt_BR.UTF-8");
+//
+//	int num;
+//	cout << "Insira um número: ";
+//	cin >> num;
+//
+//	switch (num)
+//	{
+//	case 1:
+//		cout << "Domingo";
+//		break;
+//	case 2:
+//		cout << "Segunda-Feira";
+//		break;
+//	case 3:
+//		cout << "Terça-Feira";
+//		break;
+//	case 4: 
+//		cout << "Quarta-Feira";
+//		break;
+//	case 5:
+//		cout << "Quinta-Feira";
+//		break;
+//	case 6:
+//		cout << "Sexta-Feira";
+//		break;
+//	case 7:
+//		cout << "Sábado";
+//		break;
+//	default:
+//		cout << "Dia inválido";
+//		break;
+//	}
+//
+//}
+
+
+// EXERCICIO 2
+//int main() {
+//	setlocale(LC_ALL, "pt_BR.UTF-8");
+//
+//	float num1;
+//	float num2;
+//	char operacao;
+//	int resultado;
+//
+//	cout << "Informe um número: ";
+//	cin >> num1;
+//
+//	cout << "Informe outro número: ";
+//	cin >> num2;
+//
+//	cout << "Informe a operação matemática que deseja (+ , - , * , /)";
+//	cin >> operacao;
+//
+//	switch (operacao)
+//	{
+//	case '+':
+//		resultado = num1 + num2;
+//		cout << "O resultado é: " << resultado;
+//		break;
+//	case '-':
+//		resultado = num1 - num2;
+//		cout << "O resultado é: " << resultado;
+//		break;
+//	case '*':
+//		resultado = num1 * num2;
+//		cout << "O resultado é: " << resultado;
+//		break;
+//	case '/':
+//		resultado = num1 / num2;
+//		cout << "O resultado é: " << resultado;
+//		break;
+//	default:
+//		break;
+//	}
+//	
+//
+//}
+
+//int main() {
+//	setlocale(LC_ALL, "pt_BR.UTF-8");
+//
+//	char letra;
+//	
+//	cout << "Digite uma letra:";
+//	cin >> letra;
+//
+//	switch (letra)
+//	{
+//	case 'a':
+//	case 'A':
+//		{
+//		cout << "A letra " << letra << " é uma vogal!";
+//		break;
+//		}
+//	case 'e':
+//	case 'E':
+//		{
+//		cout << "A letra " << letra << " é uma vogal!";
+//		break;
+//		}
+//	case 'i':
+//	case 'I':
+//		{
+//		cout << "A letra " << letra << " é uma vogal!";
+//		break;
+//		}
+//	case 'o':
+//	case 'O':
+//		{
+//		cout << "A letra " << letra << " é uma vogal!";
+//		break;
+//		}
+//	case 'u':
+//	case 'U':
+//		{
+//		cout << "A letra " << letra << " é uma vogal!";
+//		break;
+//		}
+//	default:
+//		cout << "A letra " << letra << " é uma consoante";
+//		break;
+//	}
+//}
